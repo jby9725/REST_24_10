652fb82a5c1b598c4d69ee821611e2f8d16fd0d0
jby9725 <jby9725@gmail.com>
2024-10-30 14:21:03 +0900

    NotProd에 의해 앱 시작시 회원 2명 자동 생성

---
+++ src/main/java/com/koreait/rest_24_10/boundedContext/member/memberRepository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.koreait.rest_24_10.boundedContext.member.memberRepository;
+
+import com.koreait.rest_24_10.boundedContext.member.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByUsername(String username);
+}
