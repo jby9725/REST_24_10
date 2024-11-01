0c8cf6eb30f4b54eb3eabdf8c78a2c16f69da522
jby9725 <jby9725@gmail.com>
2024-11-01 15:39:04 +0900

    스프링 시큐리티, 자격 없는 요청에 대한 403 응답 양식 변경

--- src/main/java/com/koreait/rest_24_10/base/security/ApiSecurityConfig.java
+++ src/main/java/com/koreait/rest_24_10/base/security/ApiSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.koreait.rest_24_10.base.security;
 
+import com.koreait.rest_24_10.base.security.entryPoint.ApiAuthenticationEntryPoint;
 import com.koreait.rest_24_10.base.security.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
@@ -17,11 +18,15 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class ApiSecurityConfig {
 
     private final JwtAuthorizationFilter jwtAuthorizationFilter;
+    private final ApiAuthenticationEntryPoint authenticationEntryPoint;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { // 기본 설정 해준거 끄기.
         http
                 .securityMatcher("/api/**") // 아래의 모든 설정은 /api/** 경로에만 적용
+                .exceptionHandling(exceptionHandling -> exceptionHandling
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                )
                 .authorizeHttpRequests(
                         authorizeRequests -> authorizeRequests
                                 .requestMatchers("/api/*/member/login").permitAll() // 로그인은 누구나 가능
